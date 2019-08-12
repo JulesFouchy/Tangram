4540751fe752d20b9911ae7c16640acfbc5eba7f
Jules Fouchy <jules.fouchy@ntymail.com>
2019-08-12 12:17:26 +0200

    Setting up GLEW

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
-#include "SDL/include/SDL.h"
-#include <stdio.h>
+#include "GL/glew.h"
+#include "SDL.h"
+#include "SDL_opengl.h"
+#include <iostream>
 
 int main(int argc, char* argv[])
 {
@@ -11,8 +13,82 @@ int main(int argc, char* argv[])
 		640, 480, // window's length and height in pixels  
 		SDL_WINDOW_OPENGL);
 
-	SDL_Delay(3000); // window lasts 3 seconds
+	SDL_GLContext context = SDL_GL_CreateContext(window);
+
+	if (glewInit() != GLEW_OK) {
+		std::cout << "Error initializing glew !" << std::endl;
+		return -1;
+	}
+
+
+	bool bQuit = false;
+	while (!bQuit) {
+
+		// Handle inputs
+		SDL_Event e;
+		while (SDL_PollEvent(&e)) {
+
+			if (e.type == SDL_QUIT) {
+				bQuit = true;
+				break;
+			}
+
+			switch (e.type) {
+			case SDL_MOUSEBUTTONUP:
+				if (e.button.button == SDL_BUTTON_LEFT)
+					;
+				else if (e.button.button == SDL_BUTTON_RIGHT)
+					;
+				break;
+
+			case SDL_MOUSEBUTTONDOWN:
+				if (e.button.button == SDL_BUTTON_LEFT)
+					;
+				else if (e.button.button == SDL_BUTTON_RIGHT)
+					;
+				break;
+
+			case SDL_MOUSEMOTION:
+				break;
+
+			case SDL_KEYDOWN:
+				std::cout << e.key.keysym.sym << std::endl;
+				if (e.key.keysym.sym == 'p') {
+
+				}
+				else if (e.key.keysym.scancode == SDL_SCANCODE_UP) {
+					;
+				}
+				else if (e.key.keysym.scancode == SDL_SCANCODE_DOWN) {
+					;
+				}
+				else if (e.key.keysym.scancode == SDL_SCANCODE_LEFT) {
+					;
+				}
+				else if (e.key.keysym.scancode == SDL_SCANCODE_RIGHT) {
+					;
+				}
+				break;
+
+			case SDL_MOUSEWHEEL:
+				//emitter.publish<evnt::MouseScrolled>((int)e.motion.x);
+				//if (e.motion.x > 0.0f) {
+				//	emitter.publish<evnt::ScaleView>(1.0f);
+				//} else {
+				//	emitter.publish<evnt::ScaleView>(-1.0f);
+				//}
+				break;
+			}
+		}
+
+		// Update window
+		SDL_GL_SwapWindow(window);
+
+	}
+
 	SDL_DestroyWindow(window);
 	SDL_Quit();
+
+
 	return 0;
 }
