c589fba9f3c9a31dda727c03d3a8aaf21ecd4840
Jules Fouchy <jules.fouchy@ntymail.com>
2019-09-26 18:28:32 +0200

    ImGui AspectRatio/Width/Height picker

--- src/UI/GUI_LayerCreation.cpp
+++ src/UI/GUI_LayerCreation.cpp
@@ -3,20 +3,82 @@
 
 #include "imgui/imgui.h"
 
-bool GUI_LayerCreation::m_bShaderLayerCreationWindow = false;
-
+bool GUI_LayerCreation::m_bWindow_ShaderLayerCreation = true;
+unsigned int GUI_LayerCreation::m_aspectRatioNumerator = 1;
+unsigned int GUI_LayerCreation::m_aspectRatioDenominator = 1;
+unsigned int GUI_LayerCreation::m_width = 1000;
+unsigned int GUI_LayerCreation::m_height = 1000;
+WidthOrHeight GUI_LayerCreation::m_lastModified = Height;
 
 void GUI_LayerCreation::Show() {
-	if (m_bShaderLayerCreationWindow)
-		ImGuiShaderLayerCreationWindow();
+	if (m_bWindow_ShaderLayerCreation)
+		Window_ShaderLayerCreation();
 }
 
 void GUI_LayerCreation::OpenCreateShaderLayerWindow() {
-	m_bShaderLayerCreationWindow = true;
+	m_bWindow_ShaderLayerCreation = true;
 }
 
-void GUI_LayerCreation::ImGuiShaderLayerCreationWindow() {
-	ImGui::Begin("Creating a ShaderLayer");
-
+void GUI_LayerCreation::Window_ShaderLayerCreation() {
+	ImGui::Begin("Creating a ShaderLayer", &m_bWindow_ShaderLayerCreation);
+	ImGuiChoose_Ratio_Width_Height();
 	ImGui::End();
+}
+
+void GUI_LayerCreation::ImGuiChoose_Ratio_Width_Height() {
+	ImGui::PushItemWidth(25.f);
+	// Aspect ratio
+	ImGui::Text("Aspect Ratio : "); ImGui::SameLine();
+	ImGui::PushID(0);
+	if (ImGui::InputScalar("", ImGuiDataType_U32, &m_aspectRatioNumerator, NULL, NULL, "%u")) {
+		updateWidthOrHeight();
+	}
+	ImGui::SameLine();
+	ImGui::PopID();
+	ImGui::Text("/"); ImGui::SameLine();
+	ImGui::PushID(1);
+	if (ImGui::InputScalar("", ImGuiDataType_U32, &m_aspectRatioDenominator, NULL, NULL, "%u")) {
+		updateWidthOrHeight();
+	}
+	ImGui::PopID();
+	ImGui::SameLine();
+	if (ImGui::Button("Same as DrawingBoard's")) {
+		spdlog::warn("This button is not really working ! ...");
+		m_aspectRatioNumerator = 3;
+		m_aspectRatioDenominator = 2;
+		updateWidthOrHeight();
+	}
+	ImGui::PopItemWidth();
+	// Width and Height
+	ImGui::PushItemWidth(100.f);
+	ImGui::Text("Width : "); ImGui::SameLine();
+	ImGui::PushID(2);
+	if (ImGui::InputScalar("", ImGuiDataType_U32, &m_width, NULL, NULL, "%u")) {
+		m_lastModified = Width;
+		updateWidthOrHeight();
+	}
+	ImGui::SameLine();
+	ImGui::PopID();
+	ImGui::Text("Height : "); ImGui::SameLine();
+	ImGui::PushID(3);
+	if (ImGui::InputScalar("", ImGuiDataType_U32, &m_height, NULL, NULL, "%u")) {
+		m_lastModified = Height;
+		updateWidthOrHeight();
+	}
+	ImGui::SameLine();
+	ImGui::PopID();
+	ImGui::PopItemWidth();
+}
+
+void GUI_LayerCreation::updateWidthOrHeight() {
+	switch (m_lastModified){
+	case Width:
+		m_height = m_width / getAspectRatio();
+		break;
+	case Height:
+		m_width = m_height * getAspectRatio();
+		break;
+	default:
+		break;
+	}
 }
--- src/UI/GUI_LayerCreation.hpp
+++ src/UI/GUI_LayerCreation.hpp
@@ -1,14 +1,30 @@
 #pragma once
 
+enum WidthOrHeight {
+	Width,
+	Height
+};
+
 class GUI_LayerCreation {
 public:
 	static void Show();
 	static void OpenCreateShaderLayerWindow();
 private:
-	static void ImGuiShaderLayerCreationWindow();
+	static void Window_ShaderLayerCreation();
+
+	static void ImGuiChoose_Ratio_Width_Height();
 
 private:
-	static bool m_bShaderLayerCreationWindow;
+	static bool m_bWindow_ShaderLayerCreation;
+
+	static unsigned int m_aspectRatioNumerator;
+	static unsigned int m_aspectRatioDenominator;
+	static unsigned int m_width;
+	static unsigned int m_height;
+	static WidthOrHeight m_lastModified;
+	static void updateWidthOrHeight();
+
+	static float getAspectRatio() { return (float) m_aspectRatioNumerator / m_aspectRatioDenominator; }
 
 private:
 	GUI_LayerCreation() = default;
